94f8402c8eedae83a3e01b924fb39bbcff340e36
BrazAc <braz0502002@gmail.com>
2025-01-21 00:09:38 -0300

    created structs to store the cube edges, implemented ortograpic projection logic

--- spinningCube-v1.c
+++ spinningCube-v1.c
@@ -1,16 +1,39 @@
 #include <SDL.h>
 #include <stdio.h>
+#include <math.h>
+#ifndef M_PI
+#define M_PI 3.141592
+#endif
 
 //gcc spinningCube-v1.c `sdl2-config --cflags --libs` -o out
-
+/*
+otographic projection logic applied
+*/
 void tornaPixelsBrancos(Uint32*, int, int);
 void tornaPixelsPretos(Uint32*, int, int);
 
-int main(){
-    //Declaracao de variaveis
-    int fechar = 0;
-    SDL_Event evento;
+typedef struct estruturaVertice3d {
+    float x;
+    float y;
+    float z;
+} estruturaVertice3d;
 
+typedef struct estruturaVertice2d {
+    float x;
+    float y;
+} estruturaVertice2d;
+
+typedef struct estruturaCubo3d {
+    estruturaVertice3d vertices[8];
+} estruturaCubo3d;
+
+typedef struct estruturaCubo2d {
+    estruturaVertice2d vertices[8];
+} estruturaCubo2d;
+
+int* multiplicaMatrizes(int*, int*);
+
+int main(){
     //Inicializando SDL e indicando o uso do subsistema de video (tem varios)
     SDL_Init(SDL_INIT_VIDEO);
 
@@ -20,6 +43,92 @@ int main(){
     int LARGURA_JANELA = displayMode.w; 
     int ALTURA_JANELA = displayMode.h;
 
+
+    //Declaracao de variaveis
+    int fechar = 0;
+    SDL_Event evento;
+
+    //-- Preparar pontos com weak perspective projection
+    estruturaCubo3d cubo3d;
+    //Pra y < 0
+    cubo3d.vertices[0].x = -1;
+    cubo3d.vertices[0].y = -1;
+    cubo3d.vertices[0].z = 1;
+
+    cubo3d.vertices[1].x = 1;
+    cubo3d.vertices[1].y = -1;
+    cubo3d.vertices[1].z = 1;
+
+    cubo3d.vertices[2].x = -1;
+    cubo3d.vertices[2].y = -1;
+    cubo3d.vertices[2].z = -1;
+
+    cubo3d.vertices[3].x = 1;
+    cubo3d.vertices[3].y = -1;
+    cubo3d.vertices[3].z = -1;
+    //Pra y > 0
+    cubo3d.vertices[4].x = -1;
+    cubo3d.vertices[4].y = 1;
+    cubo3d.vertices[4].z = 1;
+
+    cubo3d.vertices[5].x = 1;
+    cubo3d.vertices[5].y = 1;
+    cubo3d.vertices[5].z = 1;
+
+    cubo3d.vertices[6].x = -1;
+    cubo3d.vertices[6].y = 1;
+    cubo3d.vertices[6].z = -1;
+
+    cubo3d.vertices[7].x = 1;
+    cubo3d.vertices[7].y = 1;
+    cubo3d.vertices[7].z = -1;
+
+    float mtzVertices3d[8][3] = {
+        {cubo3d.vertices[0].x, cubo3d.vertices[0].y, cubo3d.vertices[0].z},
+        {cubo3d.vertices[1].x, cubo3d.vertices[1].y, cubo3d.vertices[1].z},
+        {cubo3d.vertices[2].x, cubo3d.vertices[2].y, cubo3d.vertices[2].z},
+        {cubo3d.vertices[3].x, cubo3d.vertices[3].y, cubo3d.vertices[3].z},
+        {cubo3d.vertices[4].x, cubo3d.vertices[4].y, cubo3d.vertices[4].z},
+        {cubo3d.vertices[5].x, cubo3d.vertices[5].y, cubo3d.vertices[5].z},
+        {cubo3d.vertices[6].x, cubo3d.vertices[6].y, cubo3d.vertices[6].z},
+        {cubo3d.vertices[7].x, cubo3d.vertices[7].y, cubo3d.vertices[7].z}
+    };
+
+    //Converter para duas dimensoes
+    estruturaCubo2d cubo2d;
+    int mtzVertices2d[8][2] = {
+        {cubo2d.vertices[0].x, cubo2d.vertices[0].y},
+        {cubo2d.vertices[1].x, cubo2d.vertices[1].y},
+        {cubo2d.vertices[2].x, cubo2d.vertices[2].y},
+        {cubo2d.vertices[3].x, cubo2d.vertices[3].y},
+        {cubo2d.vertices[4].x, cubo2d.vertices[4].y},
+        {cubo2d.vertices[5].x, cubo2d.vertices[5].y},
+        {cubo2d.vertices[6].x, cubo2d.vertices[6].y},
+        {cubo2d.vertices[7].x, cubo2d.vertices[7].y}
+    };
+    
+    //Criar mtzTransformacao3d_2d
+    //Calcular f (FOV VERTICAL)
+    double graus_FOV = 90;
+    double radian_FOV = graus_FOV * (M_PI / 180.0);
+    double f = ALTURA_JANELA / (2 * tan(radian_FOV));
+
+    //Criar matriz
+    int mtzTransformacaoOrtografica[3][3] = {
+        {1, 0, 0},
+        {0, 1, 0},
+        {0, 0, 0},
+    };
+
+    //Multiplicar cada vetor de mtzVertices3d pela mtzTransformacaoOrtografica
+    for (int i = 0; i < 8; i ++) {
+        int *mtzResultado;
+        mtzResultado = multiplicaMatrizes(mtzVertices2d[i], mtzTransformacaoOrtografica);
+        mtzVertices2d[i][0] = mtzResultado[0];
+        mtzVertices2d[i][1] = mtzResultado[1];
+    }
+
+    //-- Mostrar os pontos com SDL
     //Criando janela
      SDL_Window *janela = SDL_CreateWindow(
         "Spining cube window 2000",
@@ -58,7 +167,7 @@ int main(){
             fechar = 1;
             break;
         } 
-        
+
         //- Configurando e mostrando textura do background
         //Carregando pixels na textura
         SDL_UpdateTexture(texturaBackground, NULL, pixels, LARGURA_JANELA * sizeof(Uint32));
@@ -95,4 +204,9 @@ void tornaPixelsPretos(Uint32 *pixels, int LARGURA_JANELA, int ALTURA_JANELA){
     for(int i = 0; i < (LARGURA_JANELA * ALTURA_JANELA); i ++){
         pixels[i] = 0;
     }
+}
+
+int* multiplicaMatrizes(int* mtz0, int* mtz1){
+    //2x1 * 3x3
+    
 }
